902638ca24c7ace0a17420edad6680ce5eb9b97f
erinlandy <e-landy@kellogg.northwestern.edu>
2016-02-02 16:34:21 -0600

    weather_3.0

--- weather.js
+++ weather.js
@@ -5,10 +5,22 @@ $(function() {
     // Put your code here to change the "markup" variable.
     // Don't change any other code in this file. You will be sad.
 
-    // var markup = "The weather report... " + "wilsl be here when I finish my homework.";
+    var markup = "The weather report... " + "wilsl be here when I finish my homework.";
 
     var markup =  {temperature: "37.67", summary:"Mostly Cloudy", icon:"partly-cloudy-night"};
     data[markup.temperature]
+
+//     var markup = {
+//     currently: {
+//         temperature: "37.67",
+//         icon: "partly-cloudy-night",
+//         summary: "Partly Cloudy" }
+//     daily:[0,1,2,3,4,5,6,7]
+//         }
+//
+// var data = "It is currently {{currently}}. The extended forecast is {daily: [0,1,2,3]}";
+//
+// var result = Mark.up(template, context);
 
     // End of your code
 
